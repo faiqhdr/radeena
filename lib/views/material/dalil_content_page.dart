e8e73574252a9b4a7ec028077a189b0bae9a5bc6
Muhammad Faiq Haidar <muhammad.haidar@graduate.utm.my>
2024-06-15 13:48:23 +0800

    add content for dalil

--- lib/views/material/dalil_content_page.dart
+++ lib/views/material/dalil_content_page.dart
@@ -26,22 +26,71 @@ class DalilContentPage extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: width * .06),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 0),
-              child: Text(
-                "Dalil ${dalil['source']}",
-                style: textUnderTitleStyle(),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 0),
+                child: Text(
+                  "Dalil Details",
+                  style: textUnderTitleStyle(),
+                ),
               ),
-            ),
-            SizedBox(height: 25),
-            Text(
-              dalil['evidenceContent'],
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-            ),
-          ],
+              SizedBox(height: 25),
+              Text(
+                "Portion:",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: green01Color),
+              ),
+              SizedBox(height: 5),
+              Text(
+                dalil['portion'],
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 25),
+              Text(
+                "Explanation:",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: green01Color),
+              ),
+              SizedBox(height: 5),
+              Text(
+                dalil['condition'],
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 25),
+              Text(
+                "Dalil:",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: green01Color),
+              ),
+              SizedBox(height: 5),
+              Text(
+                dalil['evidenceContent'],
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 25),
+              Text(
+                "Complete Dalil:",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: green01Color),
+              ),
+              SizedBox(height: 5),
+              Text(
+                dalil['fullEvidence'],
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
         ),
       ),
     );
