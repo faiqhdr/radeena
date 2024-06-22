d092bcd474b914a7f9d69ed954c9385ac2e953ee
Muhammad Faiq Haidar <muhammad.haidar@graduate.utm.my>
2024-06-23 00:36:02 +0800

    move chatbot page to information section

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:radeena/views/menu_page.dart';
 import 'package:radeena/views/division/identification_page.dart';
 import 'package:radeena/views/history/history_page.dart';
 import 'package:radeena/views/information/library_page.dart';
-import 'package:radeena/views/chatbot/chatbot_page.dart';
+import 'package:radeena/views/information/chatbot_page.dart';
 import 'package:radeena/controllers/identification_controller.dart';
 import 'package:radeena/controllers/impediment_controller.dart';
 
