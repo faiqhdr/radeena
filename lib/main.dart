fac696772b31c16d0f9eb5d17abaa0391d7eddcb
Muhammad Faiq Haidar <muhammad.haidar@graduate.utm.my>
2024-06-30 02:24:07 +0800

    change launcher splash icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:radeena/views/menu_page.dart';
 import 'package:radeena/views/division/identification_page.dart';
 import 'package:radeena/views/history/history_page.dart';
@@ -13,6 +14,8 @@ void main() {
   ImpedimentController impedimentController =
       ImpedimentController(identificationController.deceased);
 
+  WidgetsBinding widgetBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetBinding);
   runApp(MyApp(
     identificationController: identificationController,
     impedimentController: impedimentController,
@@ -21,7 +24,7 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final IdentificationController identificationController;
   final ImpedimentController impedimentController;
   const MyApp({
@@ -29,6 +32,22 @@ class MyApp extends StatelessWidget {
     required this.identificationController,
     required this.impedimentController,
   }) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    await Future.delayed(Duration(seconds: 3));
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,13 +58,13 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: MenuPage(
-        identificationController: identificationController,
-        impedimentController: impedimentController,
+        identificationController: widget.identificationController,
+        impedimentController: widget.impedimentController,
       ),
       routes: {
         '/identificationPage': (context) => IdentificationPage(
-              controller: identificationController,
-              impedimentController: impedimentController,
+              controller: widget.identificationController,
+              impedimentController: widget.impedimentController,
             ),
         '/historyPage': (context) => HistoryPage(),
         '/libraryPage': (context) => LibraryPage(),
