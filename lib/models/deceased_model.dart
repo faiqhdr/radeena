a9e0eeb3d1537b6fb3326fd07682498c6213f6c6
Muhammad Faiq Haidar <muhammad.haidar@graduate.utm.my>
2024-07-22 23:06:55 +0700

    add comment for deceased model

--- lib/models/deceased_model.dart
+++ lib/models/deceased_model.dart
@@ -1,7 +1,11 @@
+//  Created by Muhammad Faiq Haidar on 22/07/2024.
+//  Copyright © 2024 Muhammad Faiq Haidar. All rights reserved.
+
 class DeceasedModel {
   String? gender;
-  Map<String, int> heirs = {};
+  Map<String, int> heirs = {}; // Store Heirs and Quantities
 
+  // Validate If Gender is Set
   String? validateGender() {
     if (gender == null) {
       return 'Please select the deceased\'s gender for determining the heirs.';
@@ -9,10 +13,12 @@ class DeceasedModel {
     return null;
   }
 
+  // Set Deceased's Gender
   void setGender(String gender) {
     this.gender = gender;
   }
 
+  // Update Specific Heir Quantities
   void updateHeirQuantity(String heir, int quantity) {
     heirs[heir] = quantity;
   }
