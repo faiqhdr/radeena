2dbfc7ec4c22d76bc6d8b773406386e48fd0d62b
Muhammad Faiq Haidar <muhammad.haidar@graduate.utm.my>
2024-07-22 23:03:56 +0700

    add comment for chatbot model

--- lib/models/chatbot_model.dart
+++ lib/models/chatbot_model.dart
@@ -1,8 +1,13 @@
+//  Created by Muhammad Faiq Haidar on 22/07/2024.
+//  Copyright © 2024 Muhammad Faiq Haidar. All rights reserved.
+
 class ChatbotModel {
+  // Predefined Questions and Responses
   List<Map<String, String>> predefinedQuestions = [
     {"question": "How can I help you? 🤓", "response": "How can I help you?"},
   ];
 
+  // Initial Options for User
   List<Map<String, String>> getInitialOptions() {
     return [
       {"question": "I would like to know more about Faraid’s lesson"},
@@ -10,6 +15,7 @@ class ChatbotModel {
     ];
   }
 
+  // Get Questions for Input
   List<Map<String, String>> getQuestionsForInput(
       List<Map<String, dynamic>> dataList, String key) {
     var uniqueQuestions =
