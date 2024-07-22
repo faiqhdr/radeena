814c5443a6c91d8b79abcc61abe647181209a920
Muhammad Faiq Haidar <muhammad.haidar@graduate.utm.my>
2024-07-22 23:26:26 +0700

    add comment for library and property model

--- lib/models/library_model.dart
+++ lib/models/library_model.dart
@@ -1,29 +1,40 @@
-class LibraryModel {
-  List<Map<String, dynamic>> dalilList = [];
-  List<Map<String, dynamic>> lessonList = [];
+//  Created by Muhammad Faiq Haidar on 22/07/2024.
+//  Copyright © 2024 Muhammad Faiq Haidar. All rights reserved.
 
+class LibraryModel {
+  List<Map<String, dynamic>> dalilList = []; // Store Dalil Data
+  List<Map<String, dynamic>> lessonList = []; // Store Lesson Data
+
+  // Set List of Dalil Data
   void setDalilList(List<Map<String, dynamic>> dalilData) {
     dalilList = dalilData;
   }
 
+  // Set List of Lesson Data
   void setLessonList(List<Map<String, dynamic>> lessonData) {
     lessonList = lessonData;
   }
 
+  // Get Heirs from Dalil List
   List<String> getUniqueHeirs() {
-    final heirs = dalilList.map((dalil) => dalil['heir'] as String).toSet().toList();
+    final heirs =
+        dalilList.map((dalil) => dalil['heir'] as String).toSet().toList();
     return heirs;
   }
 
+  // Get Dalil Data for Specific Heir
   List<Map<String, dynamic>> getDalilForHeir(String heir) {
     return dalilList.where((dalil) => dalil['heir'] == heir).toList();
   }
 
+  // Get Lesson Data by Title
   List<Map<String, dynamic>> getLessonsByTitle(String title) {
     return lessonList.where((lesson) => lesson['title'] == title).toList();
   }
 
+  // Get Dalil Data for Specific Heir
   Map<String, dynamic> getDalilByHeir(String heir) {
-    return dalilList.firstWhere((dalil) => dalil['heir'] == heir, orElse: () => {});
+    return dalilList.firstWhere((dalil) => dalil['heir'] == heir,
+        orElse: () => {});
   }
 }
--- lib/models/property_model.dart
+++ lib/models/property_model.dart
@@ -1,3 +1,6 @@
+//  Created by Muhammad Faiq Haidar on 22/07/2024.
+//  Copyright © 2024 Muhammad Faiq Haidar. All rights reserved.
+
 class PropertyModel {
   double amount;
   double debt;
@@ -13,18 +16,21 @@ class PropertyModel {
     required this.total,
   });
 
+  // Accessor
   double getAmount() => amount;
   double getDebt() => debt;
   double getTestament() => testament;
   double getFuneral() => funeral;
   double getTotal() => total;
 
+  // Mutator
   void setAmount(double propertyAmount) { amount = propertyAmount; }
   void setDebt(double debtAmount) { debt = debtAmount; }
   void setTestament(double testamentAmount) { testament = testamentAmount; }
   void setFuneral(double funeralAmount) { funeral = funeralAmount; }
   void setTotal(double totalAmount) { total = totalAmount; }
 
+  // Calculate Total Property Value After Deductions
   void calculateTotal() {
     double total = amount - debt - testament - funeral;
     setTotal(total < 0 ? 0 : total);
